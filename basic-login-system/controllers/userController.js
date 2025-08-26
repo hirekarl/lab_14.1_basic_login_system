4ab91176585b81a3fbf440a541327425550be8f3
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-26 15:48:12 -0400

    Implemented register.

--- basic-login-system/controllers/userController.js
+++ basic-login-system/controllers/userController.js
@@ -1,5 +1,40 @@
-const register = async (req, res) => {}
+const User = require("../models/User")
 
-const login = async (req, res) => {}
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+
+    const foundUser = User.findOne({ email: email })
+
+    if (foundUser) {
+      res
+        .status(409)
+        .json({ error: `User with email ${email} already exists.` })
+    } else {
+      const newUser = await User.create({
+        username: username,
+        email: email,
+        password: password,
+      })
+
+      const newUserWithoutPassword = await User.findById(newUser._id).select(
+        "-password"
+      )
+
+      res.status(201).json(newUserWithoutPassword)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({ error: message })
+  }
+}
+
+const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+  }
+}
 
 module.exports = { register, login }
--- basic-login-system/models/User.js
+++ basic-login-system/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = "mongoose"
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
   username: {
@@ -18,6 +19,15 @@ const userSchema = new Schema({
     required: true,
     minlength: 8,
   },
+})
+
+userSchema.pre("save", async (next) => {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  }
+
+  next()
 })
 
 const User = model("User", userSchema)
